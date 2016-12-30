64d74807a6fe0989a722472fe8f851fb6b2ba99f
Rob Lovelett <rlovelett@gmail.com>
2016-12-30 00:28:03 -0600

    added some functionality for the vote button, still needs states, push to db

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,103 +1,72 @@
-$(document).ready(function() {
-
-    $('#topdropdown').dropdown({
-        inDuration: 300,
-        outDuration: 225,
-        constrain_width: false, // Does not change width of dropdown to that of the activator
-        hover: true, // Activate on hover
-        gutter: 0, // Spacing from edge
-        belowOrigin: false, // Displays dropdown below the button
-        alignment: 'left' // Displays dropdown with edge aligned to the left of button
-    });
+$(document).ready(function () {
     var config = {
-    apiKey: "AIzaSyArprwD6qM4Z6qf8LkXaO-qBTwCwiVJSz8",
-    authDomain: "happy-medium-152501.firebaseapp.com",
-    databaseURL: "https://happy-medium-152501.firebaseio.com",
-    storageBucket: "happy-medium-152501.appspot.com",
-    messagingSenderId: "37679856259"
-  };
+        apiKey: "AIzaSyANgKTSXNrxtZrAyHCvXxo0EjIA7DZ09c8",
+        authDomain: "happy-medium-rob.firebaseapp.com",
+        databaseURL: "https://happy-medium-rob.firebaseio.com",
+        storageBucket: "happy-medium-rob.appspot.com",
+        messagingSenderId: "577408532184"
+    };
     firebase.initializeApp(config);
 
-    // googmaps api key AIzaSyBQATEiEDSZipTwdCzAE3oHxn6GwQKR5gQ
-    function myMap() {
-        var mapCanvas = $("#mapArea");
-        var mapOptions = {
-            center: new google.maps.LatLng(30.2746652, -97.742191),
-            zoom: 14,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var acOptions = {
-            types: ['establishment']
-        };
-        var newMap = new google.maps.Map(document.getElementById('mapArea'), mapOptions);
-        //autocomplete area
-        var autocomplete = new google.maps.places.Autocomplete(document.getElementById('topSearch'), acOptions);
-        // bindTo is to limit the auto-complete to the bounds of the map
-        autocomplete.bindTo('bounds', newMap);
-        var infoWindowOptions = {
-            // content: 'BC Testing!'
-        };
-        var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
-
-
-        var markerOptions = {
-            position: new google.maps.LatLng(30.274665, -97.74219)
-        };
-        var marker = new google.maps.Marker(markerOptions);
-        marker.setMap(newMap);
-
-        google.maps.event.addListener(autocomplete, 'place_changed', function() {
-            infoWindow.close();
-            var place = autocomplete.getPlace();
-            if (place.geometry.viewport) {
-                newMap.fitBounds(place.geometry.viewport);
-            } else {
-                newMap.setCenter(place.geometry.location);
-                newMap.setZoom(15);
+    var database = firebase.database();
+    //var votes = {}; //init empty votes tally object
+    
+    (function () { //init html, db
+        var txt = {
+            eng: {
+                input: {
+                    header: "Type of Location: ",
+                    name: "locationOption",
+                    options: ['Coffee Shop', "Restaurant", "Park"],
+                    btn: "Vote"
+                }
             }
-            marker.setPosition(place.geometry.location);
-            infoWindow.setContent('<div><strong>' + place.name + '</strong><br>');
-            infoWindow.open(newMap, marker);
-            google.maps.event.addListener(marker, 'click', function(e) {
-
-                infoWindow.open(newMap, marker);
-
-            });
+        };
+        
+        database.ref().child("votes").once("value", function(snapshot) {
+            var votes_exist = snapshot.val(); //check if votes object exists
+            if (!votes_exist) { //if not, initialize votes = 0 for each option
+                for(i=0; i < txt.eng.input.options.length; i++) {
+                    database.ref("votes/" + txt.eng.input.options[i]).set(0);
+                };
+            };
         });
 
-    } // end of myMap function
+        //build html
+        var form_id = "form_" + txt.eng.input.name;
+        var input_name = "input_vote_" + txt.eng.input.name;
 
-    myMap();
+        $("#locationOptions").html("<form id='" + form_id + "'></form>");
+        for (i = 0; i < txt.eng.input.options.length; i++) {
+            $("form[id='" + form_id + "']").append("<input id='input_vote_" + i + "' type='radio' name='" + txt.eng.input.name + "' value='" + txt.eng.input.options[i] + "'>" + txt.eng.input.options[i] + "</input>");
+        };
+        $("form[id='" + form_id + "']").append("<button id='voteButton' type='button' class='waves-effect waves-light btn'>Vote</button>");
+        $("#voteButton").attr('disabled', true); //disable button init
+    
+    })(); //run once
 
+    //get updating values of vote tally
+    // ...
+    // ...
 
-    // < div class = "chip" >
-    // Tag <i class = "close material-icons" > close < /i> <
-    // /div>
+        $("input[name='locationOption']").click(function() {
+            if($("#voteButton").attr('disabled', true)) {
+                $("#voteButton").attr('disabled', false); //enable vote button 
+            };
+        });
 
-
-    $(document).on("click", "#submitButton", function() {
-        var address1 = $("#topSearch").val().trim();
-        console.log(address1);
-        var newAddress = $("<li>");
-        var newDiv = $("<div>");
-        var newIcon = $("<i>");
-
-        newDiv.addClass("chip");
-        // adding the "x" next to starting locations
-        newIcon.addClass("close material-icons");
-        newIcon.text("close");
-        newDiv.text(address1);
-        newDiv.append(newIcon);
-        newAddress.html(newDiv);
-        // newAddress.text(address1);
-
-        $("#startingLocationArea").prepend(newAddress);
-        // empty input box after submit is clicked
-        $("#topSearch").val("");
-    });
-
-
-
-
-
-}); // end of document ready
+        $("#voteButton").click(function() {
+            if ($("input[name='locationOption']").is(":checked")) {
+                var val = $("input:radio[name=locationOption]:checked").val(); console.log("value: " + val);
+                            
+                //push to db
+                //database.ref("votes/" + val).set();
+                //change text of vote button to voted, disable
+                $("#voteButton").attr('disabled', true); //enable vote button
+                voted = true; //set flag for voted 
+            } else {
+                console.log("NOT CHECKED");
+            };
+        });
+        
+});
