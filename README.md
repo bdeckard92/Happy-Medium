ef5b732a90f3bdaa2aad498325824107fc3b80b8
Rob Lovelett <rlovelett@gmail.com>
2017-01-05 17:50:25 -0600

    trying to get votes to push to db

--- README.md
+++
@@ -1,3 +0,0 @@
-[![Stories in Ready](https://badge.waffle.io/bdeckard92/Happy-Medium.png?label=ready&title=Ready)](https://waffle.io/bdeckard92/Happy-Medium)
-# Happy-Medium
-Group project for Brian, Wellington, Rob, and Bob
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,8 +9,7 @@ $(document).ready(function () {
     firebase.initializeApp(config);
 
     var database = firebase.database();
-    //var votes = {}; //init empty votes tally object
-    
+
     (function () { //init html, db
         var txt = {
             eng: {
@@ -22,11 +21,12 @@ $(document).ready(function () {
                 }
             }
         };
-        
-        database.ref().child("votes").once("value", function(snapshot) {
+
+        //init db
+        database.ref().child("votes").once("value", function (snapshot) {
             var votes_exist = snapshot.val(); //check if votes object exists
             if (!votes_exist) { //if not, initialize votes = 0 for each option
-                for(i=0; i < txt.eng.input.options.length; i++) {
+                for (i = 0; i < txt.eng.input.options.length; i++) {
                     database.ref("votes/" + txt.eng.input.options[i]).set(0);
                 };
             };
@@ -42,31 +42,42 @@ $(document).ready(function () {
         };
         $("form[id='" + form_id + "']").append("<button id='voteButton' type='button' class='waves-effect waves-light btn'>Vote</button>");
         $("#voteButton").attr('disabled', true); //disable button init
-    
+
     })(); //run once
 
     //get updating values of vote tally
     // ...
     // ...
 
-        $("input[name='locationOption']").click(function() {
-            if($("#voteButton").attr('disabled', true)) {
-                $("#voteButton").attr('disabled', false); //enable vote button 
-            };
-        });
+    $("input[name='locationOption']").click(function () {
+        if ($("#voteButton").attr('disabled', true)) {
+            $("#voteButton").attr('disabled', false); //enable vote button 
+        };
+    });
 
-        $("#voteButton").click(function() {
-            if ($("input[name='locationOption']").is(":checked")) {
-                var val = $("input:radio[name=locationOption]:checked").val(); console.log("value: " + val);
-                            
-                //push to db
-                //database.ref("votes/" + val).set();
-                //change text of vote button to voted, disable
-                $("#voteButton").attr('disabled', true); //enable vote button
-                voted = true; //set flag for voted 
-            } else {
-                console.log("NOT CHECKED");
-            };
-        });
-        
+    $("#voteButton").click(function () {
+        if ($("input[name='locationOption']").is(":checked")) {
+            var val = $("input:radio[name=locationOption]:checked").val();
+            //var votes = {};
+            //console.log("value: " + val + "\n" + "votes: " + votes);
+           
+            database.ref().child("votes").once("value", function (snapshot) {
+                   console.log(snapshot.child.val());
+                /*
+                var votes_exist = snapshot.val(); //check if votes object exists
+                if (!votes_exist) { //if not, initialize votes = 0 for each option
+                    for (i = 0; i < txt.eng.input.options.length; i++) {
+                        database.ref("votes/" + txt.eng.input.options[i]).set(0);
+                    };
+                };
+                */
+            });
+
+            //push to db
+            //database.ref("votes/" + val).set();
+            $("input:radio[name=locationOption]").attr('disabled', true); //disable radio buttons
+            $("#voteButton").html("Voted").attr('disabled', true); //change text of vote button to voted, disable
+        };
+    });
+
 });
